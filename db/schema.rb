e821710420fa1b1608edb9d657fec3b99be7392e
mohd497 <likesteel@gmail.com>
2019-02-17 05:39:51 -0800

    generate scaffolding items

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_132744) do
+ActiveRecord::Schema.define(version: 2019_02_17_133757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "body"
+    t.bigint "todo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_id"], name: "index_items_on_todo_id"
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_02_17_132744) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "todos"
 end
